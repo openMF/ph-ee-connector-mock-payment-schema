1db500258ea2375e62842ecdd44c86611e6e6668
Ishan Ranasingh <44061541+truthfool@users.noreply.github.com>
2023-08-16 16:26:47 +0530

    changed final class (#9)

--- src/main/java/org/mifos/connector/mockpaymentschema/MockPaymentSchemaApplication.java
+++ src/main/java/org/mifos/connector/mockpaymentschema/MockPaymentSchemaApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class MockPaymentSchemaApplication {
-
-    private MockPaymentSchemaApplication() {}
+public class MockPaymentSchemaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MockPaymentSchemaApplication.class, args);
