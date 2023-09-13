803347b5d0c457f97d0d3117a215a918d4585774
apurbraj <apurb@fynarfin.io>
2023-09-13 15:53:15 +0530

    Callback certificate

--- src/main/java/org/mifos/connector/mockPaymentSchema/service/SendCallbackService.java
+++ src/main/java/org/mifos/connector/mockPaymentSchema/service/SendCallbackService.java
@@ -1,13 +1,17 @@
 package org.mifos.connector.mockpaymentschema.service;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SendCallbackService {
 
     public void sendCallback(String body, String callbackURL) {
-        RestAssured.given().baseUri(callbackURL).contentType(ContentType.JSON).body(body).when().post();
+        RequestSpecification requestSpec = new RequestSpecBuilder().build();
+        requestSpec.relaxedHTTPSValidation();
+        RestAssured.given(requestSpec).baseUri(callbackURL).contentType(ContentType.JSON).body(body).when().post();
     }
 }
