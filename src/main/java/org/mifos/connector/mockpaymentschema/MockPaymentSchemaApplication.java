10427ddd91ee2c890d472fb6d5bd007fcdcd7003
dhruvsonagara <dhruvsonagara@gmail.com>
2023-09-22 17:11:04 +0530

    Hidden checkstyle warning for main class for wrong treatment of class

--- src/main/java/org/mifos/connector/mockpaymentschema/MockPaymentSchemaApplication.java
+++ src/main/java/org/mifos/connector/mockpaymentschema/MockPaymentSchemaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("org.mifos.connector.mockpaymentschema")
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 public class MockPaymentSchemaApplication {
 
     public static void main(String[] args) {
