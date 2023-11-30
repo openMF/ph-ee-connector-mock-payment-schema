d93b06ae46d9971d2d0b34371c4ee711b2299dfa
Apurb Rajdhan <103931815+apurbraj@users.noreply.github.com>
2023-11-30 08:49:12 +0530

    Mock Apis

--- src/main/java/org/mifos/connector/mockpaymentschema/api/BatchApi.java
+++ src/main/java/org/mifos/connector/mockpaymentschema/api/BatchApi.java
@@ -2,13 +2,16 @@ package org.mifos.connector.mockpaymentschema.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.mifos.connector.mockPaymentSchema.schema.BatchDTO;
+import org.mifos.connector.mockPaymentSchema.schema.BatchDetailResponse;
+import org.mifos.connector.mockPaymentSchema.service.BatchService;
 import org.mifos.connector.mockpaymentschema.schema.AuthorizationRequest;
-import org.mifos.connector.mockpaymentschema.service.BatchService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,5 +43,16 @@ public class BatchApi {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/batches/{batchId}}/summary")
+    public BatchDTO batchSummary(@PathVariable String batchId) {
+        return batchService.getBatchSummary(batchId);
+    }
+
+    @GetMapping("/batches/{batchId}/detail")
+    public BatchDetailResponse batchDetail(@PathVariable String batchId, @RequestParam(value = "pageNo", defaultValue = "0") Integer pageNo,
+                                           @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize) {
+        return batchService.getBatchDetails(batchId, pageNo, pageSize);
     }
 }
--- src/main/java/org/mifos/connector/mockpaymentschema/service/BatchService.java
+++ src/main/java/org/mifos/connector/mockpaymentschema/service/BatchService.java
@@ -1,8 +1,18 @@
-package org.mifos.connector.mockpaymentschema.service;
+package org.mifos.connector.mockPaymentSchema.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import org.mifos.connector.mockPaymentSchema.schema.BatchDTO;
+import org.mifos.connector.mockPaymentSchema.schema.BatchDetailResponse;
+import org.mifos.connector.mockPaymentSchema.schema.Transfer;
+import org.mifos.connector.mockPaymentSchema.schema.TransferStatus;
 import org.mifos.connector.mockpaymentschema.schema.AuthorizationRequest;
 import org.mifos.connector.mockpaymentschema.schema.AuthorizationResponse;
 import org.slf4j.Logger;
@@ -19,9 +29,16 @@ public class BatchService {
     private String thresholdAmount;
 
     @Autowired
-    private SendCallbackService sendCallbackService;
+    private org.mifos.connector.mockpaymentschema.service.SendCallbackService sendCallbackService;
 
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private String payerPartyId = "835322416";
+
+    private String payeePartyId = "27713803912";
+
+    private int successTxnCount = 9;
+
 
     @Async("asyncExecutor")
     public void getAuthorization(String batchId, String clientCorrelationId, AuthorizationRequest authRequest, String callbackUrl) {
@@ -42,4 +59,76 @@ public class BatchService {
             logger.error(e.toString());
         }
     }
+
+
+    public BatchDTO getBatchSummary(String batchId) {
+        return successfulBatchSummaryResponse(batchId);
+    }
+
+
+    public BatchDetailResponse getBatchDetails(String batchId, int pageNo, int pageSize) {
+        List<Transfer> transactions = getTransactions(batchId);
+        int toIndex = pageNo * pageSize;
+        int fromIndex = toIndex - pageSize;
+        toIndex = Math.min(toIndex, transactions.size());
+        BatchDetailResponse batchDetailResponse = new BatchDetailResponse();
+        batchDetailResponse.setContent(transactions.subList(fromIndex, toIndex));
+        return batchDetailResponse;
+    }
+
+    private BatchDTO successfulBatchSummaryResponse(String batchId) {
+        Long total = 10L;
+        Long ongoing = 0L;
+        Long failed = 1L;
+        Long successful = 9L;
+        BigDecimal totalAmount = BigDecimal.valueOf(100);
+        BigDecimal ongoingAmount = BigDecimal.valueOf(0);
+        BigDecimal failedAmount = BigDecimal.valueOf(10);
+        BigDecimal successfulAmount = BigDecimal.valueOf(90);
+        String status = "Pending";
+        String successPercentage = "90";
+        String failedPercentage = "10";
+
+        return new BatchDTO(batchId, null, total, ongoing, failed, successful, totalAmount, successfulAmount,
+                ongoingAmount, failedAmount, null, null, null, status, null, null,
+                failedPercentage, successPercentage);
+    }
+
+    private List<Transfer> getTransactions(String batchId) {
+        List<Transfer> transactionList = new ArrayList<>();
+
+        for (int index = 0; index < 10; index++) {
+            Transfer transfer;
+
+            if (successTxnCount > 0) {
+                transfer = getSingleTransaction(index, UUID.randomUUID().toString(), UUID.randomUUID().toString(),
+                        TransferStatus.COMPLETED,
+                        batchId);
+            } else {
+                transfer = getSingleTransaction(index, UUID.randomUUID().toString(), UUID.randomUUID().toString(),
+                        TransferStatus.IN_PROGRESS,
+                        batchId);
+            }
+            transactionList.add(transfer);
+        }
+        return transactionList;
+    }
+
+    private Transfer getSingleTransaction(int index, String workflowInstanceKey, String requestId, TransferStatus status,
+                                          String batchId) {
+        String id = String.valueOf(index);
+        Date startedAt = new Date(1685536200000L);
+        Date completedAt = new Date(1685536268000L);
+        String payerPartyIdType = "MSISDN";
+        String payeePartyIdType = "MSISDN";
+        BigDecimal amount = BigDecimal.valueOf(10);
+        String currency = "USD";
+        String direction = "OUTGOING";
+
+        return new Transfer(id, Long.parseLong(workflowInstanceKey), requestId, startedAt, completedAt, status,
+                null, null, payeePartyId, payeePartyIdType, null, null,
+                null, null, payerPartyId, payerPartyIdType, null, null,
+                null, amount, currency, direction, null, batchId, null);
+    }
+
 }
