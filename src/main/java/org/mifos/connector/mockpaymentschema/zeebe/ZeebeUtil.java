7803517321d6ab46f4af04b5ea57355c7d7e05bc
Anover000 <ankit@fynarfin.io>
2024-01-31 01:48:23 +0530

    remove camel completely

--- src/main/java/org/mifos/connector/mockpaymentschema/zeebe/ZeebeUtil.java
+++ src/main/java/org/mifos/connector/mockpaymentschema/zeebe/ZeebeUtil.java
@@ -1,8 +1,6 @@
 package org.mifos.connector.mockpaymentschema.zeebe;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Map;
-import org.apache.camel.Exchange;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,28 +11,4 @@ public final class ZeebeUtil {
     private static Logger logger = LoggerFactory.getLogger(ZeebeUtil.class);
     private static ObjectMapper objectMapper = new ObjectMapper();
 
-    public static void zeebeVariablesToCamelProperties(Map<String, Object> variables, Exchange exchange, String... names) {
-        exchange.setProperty("zeebeVariables", variables);
-
-        for (String name : names) {
-            Object value = variables.get(name);
-            if (value == null) {
-                logger.error("failed to find Zeebe variable name {}", name);
-            } else {
-                exchange.setProperty(name, value);
-            }
-        }
-    }
-
-    public static Map<String, Object> zeebeVariablesFrom(Exchange exchange) {
-        return exchange.getProperty("zeebeVariables", Map.class);
-    }
-
-    public static <T> T zeebeVariable(Exchange exchange, String name, Class<T> clazz) throws Exception {
-        Object content = zeebeVariablesFrom(exchange).get(name);
-        if (content instanceof Map) {
-            return objectMapper.readValue(objectMapper.writeValueAsString(content), clazz);
-        }
-        return (T) content;
-    }
 }
